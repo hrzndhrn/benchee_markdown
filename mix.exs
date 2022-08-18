e0b7fb7ea0c92513d790b2b204bd872d24a4a1aa
Marcus Kruse <esurk.sucram@gmail.com>
2022-08-18 21:13:51 +0200

    Bump Elixir to 1.7

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule BencheeMarkdown.MixProject do
     [
       app: :benchee_markdown,
       version: @version,
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
